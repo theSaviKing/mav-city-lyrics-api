db39d899ba3fcb78072372de384f9753a589fedc
theSaviKing <tpaulkariuki@gmail.com>
2022-08-09 09:53:19 -0500

    moved app folder contents to main file

--- lyrics.py
+++ lyrics.py
@@ -1,1 +1,13 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/lyrics')
+def index():
+    return render_template('index.html')
+
+@app.route('/add')
+def add_lyrics():
+    return render_template('add.html')
