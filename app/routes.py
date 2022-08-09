8348cc27b6fc6fe4ae69788e7f3cd3ece2a31c46
theSaviKing <tpaulkariuki@gmail.com>
2022-08-09 09:33:54 -0500

    initialize tailwind and styling

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 @app.route('/lyrics')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/add')
+def add_lyrics():
+    return render_template('add.html')
